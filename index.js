3f2873f513369861f889ef8e58195b89cabcb0ac
stoganych <stoganych@gmail.com>
2024-07-11 11:35:19 +0300

    init commit

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var express = require('express');
-var cors = require('cors');
+const multer  = require('multer');
+const bodyParser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
+const upload = multer();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+})
 
 
 
